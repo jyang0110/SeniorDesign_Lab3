daf09ad3e0911e5b98a53625345f7063cc7ed677
Pengyang Xiang <xiangpengyang.mun@gmail.com>
2018-04-25 11:26:22 -0500

    Update frequency_estimate.m

--- frequency_estimate.m
+++ frequency_estimate.m
@@ -1,9 +1,9 @@
-function freq_estimator = estimate(audio,Ts)
-sigphase=angle((audio).^2);
-tsamp_n=Ts:Ts:Ts*length(audio);
+function freq_est = frequency_estimate(sample,Ts)
+sigphase=angle((sample).^2); % phase of the squared signal
+tsamp_n=Ts:Ts:Ts*length(sample); % time values for the sample
 
-uwphase=unwrap(sigphase);
+uwphase=unwrap(sigphase); % unwrap the phase so the slope can be calculated
 
 p=polyfit(tsamp_n,uwphase,1); % fit a polynomial of degree 1 i.e. a line
 
-freq_est=p(1)/(2*pi*2);
+freq_est=p(1)/(2*pi*2); % the frequency error is one half the slope of the phase
