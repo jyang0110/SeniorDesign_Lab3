b83ab36204d070f7b743f942f91497bc61569e21
Pengyang Xiang <xiangpengyang.mun@gmail.com>
2018-04-26 10:10:40 -0500

    Update lab3.m

--- lab3.m
+++ lab3.m
@@ -19,7 +19,7 @@ N_buf = floor(T_buf/Ts);
 
 n=0;
 p=0;
-undistorted = zeros(1,N); %create a 1-by-N matrix
+undistorted = zeros(1,N); %create a 1-by-N matrix of all zeros
 ffreq_estimate = 0;
 
 % now sit in a loop and process blocks of samples until we run out
